08b3e3f00d257ac1090deb7ca63c3cd6db8b0140
Abhay Pratap Singh <abhi98787898@gmail.com>
2025-10-08 14:13:18 +0000

    variables

---
+++ o1_basics/01_variables.js
@@ -0,0 +1,11 @@
+var accountPassward = "98787898"
+let accountEmail = "abhi98787898@gmail.com"
+const accountId = "53984"
+// const me dubara value assign nhi kar sakte
+
+/*
+prefer not to use var 
+because of issue in block scope and functional scope
+*/
+
+console.log(accountId);
