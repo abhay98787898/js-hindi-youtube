4e68db0ef7e53a48aafc89b4688e3106186a19e5
Abhay Pratap Singh <abhi98787898@gmail.com>
2025-10-08 14:13:45 +0000

    datatypes

---
+++ o1_basics/02_dataTypes.js
@@ -0,0 +1,14 @@
+"use strict"; //treat all js codes like newer version
+
+//number
+//bigint
+//string
+//boolian => true/false
+//null => standalone value
+//undefined
+//symbol => unique
+//object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null);  //object
+
